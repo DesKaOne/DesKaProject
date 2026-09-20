25abdf936a13474da52d14b05bb562191d514fd2
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-20 22:00:04 +0700

    phase-7: make handshake use resolved network identity

--- deskachain/node/internal/p2p/server.go
+++ deskachain/node/internal/p2p/server.go
@@ -160,7 +160,6 @@ func (s Server) handshake(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	net := s.network()
-	net.GenesisHash = chain.GenesisHashForNetwork(net)
 	challenge := strings.TrimSpace(r.URL.Query().Get("challenge"))
 	if challenge != "" {
 		if len(challenge) != 64 {
