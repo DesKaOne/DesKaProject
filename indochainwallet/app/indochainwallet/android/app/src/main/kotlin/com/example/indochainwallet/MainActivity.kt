64446c8bd962a0fe2ef137ed797b04dce7a8fa1a
Antoni Oktha Fernandes <37358597+DesKaOne@users.noreply.github.com>
2026-09-21 19:57:46 +0700

    Add initial iOS project structure and assets for Indochainwallet
    - Created iOS project structure with necessary files including Info.plist, LaunchScreen.storyboard, and Main.storyboard.
    - Added app icons in various resolutions to Assets.xcassets.
    - Implemented LaunchImage assets for the launch screen.
    - Set up Flutter main application entry point in main.dart.
    - Included basic widget test for counter functionality.
    - Established Go module for backend with go.mod.

---
+++ indochainwallet/app/indochainwallet/android/app/src/main/kotlin/com/example/indochainwallet/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.indochainwallet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ indochainwallet/backend/go.mod
@@ -0,0 +1,3 @@
+module indochainwallet
+
+go 1.25.1
